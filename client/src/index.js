6ff3a8c2162ea08a04ef0e36ed8840b1ba3074da
theo tran <theotran@rocketmail.com>
2018-02-01 17:07:36 -1000

    created mockNetworkInterface to use but not compatible with the new migration standards of Apollo

--- client/src/index.js
+++ client/src/index.js
@@ -10,9 +10,24 @@ import {
 } from 'react-apollo';
 import gql from 'graphql-tag';
 import { ApolloClient } from 'apollo-client';
+import { createHttpLink } from 'apollo-link-http';
+import { InMemoryCache } from 'apollo-cache-inmemory';
+
+import {
+  makeExecutableSchema,
+  addMockFunctionsToSchema
+} from 'graphql-tools';
+import { mockNetworkInterfaceWithSchema } from 'apollo-test-utils';
+import { typeDefs } from './schema';
+
+const schema  = makeExecutableSchema({ typeDefs });
+addMockFunctionsToSchema({ schema });
+
+const mockNetworkInterface = mockNetworkInterfaceWithSchema({ schema });
 
 const client = new ApolloClient({
-
+  link: createHttpLink({ uri: 'http://localhost:3000' }),
+  cache: new InMemoryCache()
 });
 
 const WrappedApp = (
@@ -20,7 +35,6 @@ const WrappedApp = (
     <App />
   </ApolloProvider>
 )
-
 
 ReactDOM.render(WrappedApp, document.getElementById('root'));
 registerServiceWorker();
